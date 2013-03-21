0935c25e675facf69851611bf7ca1aa6419fc3d3
Tom Knapen <tom@failproof.be>
2013-03-21 22:23:25 +0100

    added license info

--- concat_values.hpp
+++ concat_values.hpp
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #ifndef _CONCAT_VALUES_HPP
 #define _CONCAT_VALUES_HPP
 
--- mysql_basic_row.hpp
+++ mysql_basic_row.hpp
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #ifndef _MYSQL_BASIC_ROW_HPP
 #define _MYSQL_BASIC_ROW_HPP
 
