b45805165c4446788a0ef85417d9e467a985b111
Tom Knapen <tom@failproof.be>
2013-03-21 15:59:24 +0100

    added type identifier to field

--- field.hpp
+++ field.hpp
@@ -5,7 +5,19 @@ namespace fp {
 
     template<typename TDescriptor, int I> struct field {
         enum { index = I };
+        typedef typename TDescriptor::template field<I>::type type;
+        static char const * name;
     };
+    template<typename TDescriptor, int I> char const * field<TDescriptor, I>::name = TDescriptor::template field<I>::name;
+    
+    template<typename TDescriptor, int I>
+    char const * get_field_identifier() {
+        return field<TDescriptor, I>::name;
+    }
+    template<typename TDescriptor, int I>
+    char const * get_field_identifier(field<TDescriptor, I>) {
+        return field<TDescriptor, I>::name;
+    }
 }
 
 #endif
